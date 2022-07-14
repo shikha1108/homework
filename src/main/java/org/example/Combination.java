ff091709ccc34867deb2db2e88be5056ae01960a
shikha <shikha.nick011@gmail.com>
2022-07-14 23:19:53 +0530

    todays Combination

--- src/main/java/org/example/Combination.java
+++ src/main/java/org/example/Combination.java
@@ -16,7 +16,7 @@ public class Combination {
     private static void combination(List<String> strs){
         for (int i = 0; i < strs.size() ; i = i + 1) {
             for (int j = j + 1; j < strs.size() ; j++) {
-                System.out.println(strs.get(i) + " ");
+                System.out.print(strs.get(i) + " ");
                 System.out.println(strs.get(j));
 
             }
